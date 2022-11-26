5add877a89b86b20f50e3dc316d0c903766dbf01
Mauricio Sartori Mota <106759805+jugimaujo@users.noreply.github.com>
2022-11-26 18:37:33 -0300

    Add files via upload

---
+++ pset4/filter/helpers.c
@@ -0,0 +1,40 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed, green = image[i][j].rgbtGreen, blue = image[i][j].rgbtBlue;
+            
+            if ((red == 231 && green == 230 && blue == 230) || (red == 225 && green == 212 && blue == 178))
+            {
+                image[i][j].rgbtRed = 255;
+                image[i][j].rgbtGreen = 255;
+                image[i][j].rgbtBlue = 255;
+            }
+        }
+    }
+    
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
